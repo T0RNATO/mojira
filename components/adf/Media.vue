7ddbcf50c72735cd6b505832e147a00d00815684
T0RNATO <nlittle315@gmail.com>
2025-04-02 09:35:04 +1100

    feat: fix up media gallery a bit

--- components/adf/Media.vue
+++ components/adf/Media.vue
@@ -3,21 +3,61 @@ import type {MediaNode} from "~/components/adf/types";
 import type {Issue} from "~/server/api/issue";
 import MediaItem from "~/components/adf/MediaItem.vue";
 
-defineProps<{
+const props = defineProps<{
     node: MediaNode
     att: Issue["attachments"]
 }>();
 
+function modal(ev: KeyboardEvent) {
+    if (modalOpen.value) {
+        switch (ev.key) {
+            case "Escape": {
+                modalOpen.value = false;
+                break;
+            }
+            case "ArrowLeft": {
+                galleryIndex.value--;
+                break;
+            }
+            case "ArrowRight": {
+                galleryIndex.value++;
+                console.log(galleryIndex.value);
+                break;
+            }
+        }
+    }
+    if (galleryIndex.value === props.node.content.length) {
+        galleryIndex.value = 0;
+    }
+    if (galleryIndex.value === -1) {
+        galleryIndex.value = props.node.content.length - 1;
+    }
+}
+
+onMounted(() => {
+    document.body.addEventListener("keyup", modal);
+})
+onUnmounted(() => {
+    document.body.removeEventListener("keyup", modal);
+})
+
+const galleryIndex = ref(0);
+const modalOpen = ref(false);
 </script>
 
 <template>
     <div v-if="node.type === 'mediaGroup'" class="flex flex-wrap gap-2">
-        <div v-for="media in node.content">
-            <MediaItem :media="media" :att="att"/>
+        <div v-for="(media, i) in node.content">
+            <MediaItem :media="media" :att="att" @open="modalOpen = true; galleryIndex = i"/>
         </div>
     </div>
     <div v-else>
-        <MediaItem :media="node.content[0]" :att="att"/>
+        <MediaItem :media="node.content[0]" :att="att" @open="modalOpen = true; galleryIndex = 0"/>
+    </div>
+    <div class="fixed w-screen h-screen top-0 left-0 bg-black/30 flex items-center justify-center backdrop-blur-xs z-10"
+         v-if="modalOpen" @click="modalOpen = false"
+    >
+        <MediaItem :media="node.content[galleryIndex]" :att="att" :gallery="true" class="!min-w-[80%] !min-h-[80%]" @click.stop/>
     </div>
 </template>
 
--- components/adf/MediaItem.vue
+++ components/adf/MediaItem.vue
@@ -4,60 +4,42 @@ import type {Media} from "~/components/adf/types";
 const props = defineProps<{
     media: Media
     att: Issue["attachments"]
+    gallery?: boolean
 }>()
+
+defineEmits(['open']);
 
 if (!props.att) {
     throw Error("Attempting to render media attachment but no attachments found");
 }
 
-const attachment = props.att[props.media.attrs.id];
+const attachment = computed(() => props.att![props.media.attrs.id]);
 
 const doc = ref('');
 
-if (attachment.type === "doc") {
-    fetch(attachment.url).then(res => res.text().then(d => {
-        doc.value = d;
-    }))
-}
-
-function closeModal(ev: KeyboardEvent) {
-    if (ev.key === "Escape") {
-        modalOpen.value = false;
+watch(attachment, (value) => {
+    if (value.type === "doc") {
+        fetch(value.url).then(res => res.text().then(d => {
+            doc.value = d;
+        }))
     }
-}
-
-onMounted(() => {
-    document.body.addEventListener("keyup", closeModal);
-})
-onUnmounted(() => {
-    document.body.removeEventListener("keyup", closeModal);
+}, {
+    immediate: true,
 })
 
-const modalOpen = ref(false);
 const videoPlayerVisible = ref(false);
 </script>
 
 <template>
-    <div v-if="attachment.type === 'video'" class="relative" @click="videoPlayerVisible = true">
-        <video :src="attachment.url" class="inline-block w-80 align-baseline" :controls="videoPlayerVisible ? 'controls' : false" @blur="videoPlayerVisible = false"/>
-        <img class="absolute top-1/2 left-1/2 -translate-1/2 w-16 h-16 rotate-90 select-none" draggable="false" src="/play_button.svg" v-if="!videoPlayerVisible">
+    <div v-if="attachment.type === 'video'" class="relative w-80" @click="videoPlayerVisible = true">
+        <video :src="attachment.url" class="inline-block w-full align-baseline" :controls="videoPlayerVisible ? 'controls' : false" @blur="videoPlayerVisible = false"/>
+        <img class="absolute top-1/2 left-1/2 -translate-1/2 w-16 h-16 rotate-90 select-none" draggable="false" src="/play_button.svg" v-if="!videoPlayerVisible" alt="Play Button">
     </div>
-    <img v-else-if="attachment.type === 'image'" :src="attachment.url" :alt="attachment.name" class="inline-block w-80 align-baseline" @click="modalOpen = true">
+    <img v-else-if="attachment.type === 'image'" :src="attachment.url" :alt="attachment.name" class="inline-block w-80 align-baseline" @click="$emit('open')">
     <div v-else-if="attachment.type === 'archive'" class="window">File Download: <span class="rounded-sm bg-black/20">{{attachment.name}}</span> (Unsupported)</div>
-    <div v-else-if="attachment.type === 'doc'" @click="modalOpen = true" class="relative inline-block w-80 h-60 p-4 bg-c1 rounded-lg">
-        <pre class="overflow-hidden h-50">{{doc}}</pre>
+    <div v-else-if="attachment.type === 'doc'" @click="$emit('open')" class="relative inline-block w-80 h-60 p-4 bg-c1 rounded-lg">
+        <pre :class="['h-full overflow-auto', {'overflow-hidden': !gallery}]">{{doc}}</pre>
         <div class="absolute left-0 bottom-0 w-full bg-c3 rounded-b-lg p-4">{{attachment.name}}</div>
     </div>
     <div v-else>Unsupported attachment type "{{attachment.type}}": <a :href="attachment.url">{{attachment.name}}</a>.</div>
-
-    <div class="fixed w-screen h-screen top-0 left-0 bg-black/30 flex items-center justify-center backdrop-blur-xs z-10"
-         v-if="modalOpen" @click="modalOpen = false"
-    >
-        <img v-if="attachment.type === 'image'" :src="attachment.url" :alt="attachment.name" class="max-w-[90%] max-h-[90%] min-w-[70%]" @click.stop>
-        <pre v-else-if="attachment.type === 'doc'" class="w-[80%] h-[80%] window overflow-auto" @click.stop>{{doc}}</pre>
-    </div>
 </template>
-
-<style scoped>
-
-</style>
